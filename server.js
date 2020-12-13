21a85e8270c4fc60d56e6c7213aa023f3759dbb0
Jenny <finej01@gmail.com>
2020-12-13 13:00:21 -0500

    testing

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ const app = express();
 
 // MIDDLEWARE ////////////////////////////////////////////////////////////
 app.use(express.urlencoded({ extended: false }));
-app.use('/', routes);
+//app.use('/', routes);
+
+
+
+app.get('/', authorize)
+
+
+function authorize(req, res){
+
+    //but when do i send the data???
+res.redirect('http://localhost:3000');
+
+}
 
 // LISTEN ////////////////////////////////////////////////////////////////
 app.listen(process.env.PORT || 5000);
